5e582bff7bbaa30603cb702419a62758d76ad742
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-08-02 18:11:00 -0300

    ok

--- Calculadora/assets/js/main.js
+++ Calculadora/assets/js/main.js
@@ -1,32 +1,41 @@
+const display = document.querySelector('.display');
+const operators = ['+', '-', '*', '/'];
+
+const isOperator = (char) => {
+    return operators.includes(char);
+}
 
 const addElement = (el) => {
     const display = document.querySelector('.display');
-    const text = display.value
-    if (text.endsWith('+')) removeElement();
-    if (text.endsWith('-')) removeElement();
-    if (text.endsWith('*')) removeElement();
-    if (text.endsWith('/')) removeElement();
-    display.value += el
+    if (display.value === 'Operação invalida' || display.value === 'Error') return display.value = el;
+    if (isOperator(el)) {
+        const lastChar = display.value[display.value.length - 1];
+        if (isOperator(lastChar)) {
+            display.value = display.value.slice(0, -1);
+        }
+    }
+    display.value += el;
 }
 
 const removeElement = () => {
-    const display = document.querySelector('.display');
     const text = display.value
     display.value = text.slice(0, -1)
 }
 
 const cleanDisplay = () => {
-    const display = document.querySelector('.display')
     display.value = ''
 }
 
-"use strict"
 const equal = () => {
-    const display = document.querySelector('.display');
-    try {
-        display.value = eval(display.value)
-    } catch {
-        display.value = 'Error'
+    if (!display.value) {
+        display.value = 'Operação invalida'
+    } else {
+        try {
+            'use strict';
+            display.value = eval(display.value);
+        } catch {
+            display.value = 'Error';
+        }
     }
 }
 
