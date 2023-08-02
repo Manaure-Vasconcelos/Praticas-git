6ea8011665e1c980cf3748c3bf6613bf8c4a45cc
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-08-02 13:24:45 -0300

    att

--- Calculadora/assets/js/main.js
+++ Calculadora/assets/js/main.js
@@ -1,8 +1,29 @@
-(function(){
 
-    document.addEventListener('click', (e) => {
-        const el = e.target;
+const addElement = (el) => {
+    const display = document.querySelector('.display');
+    const text = display.value
+    let lastElement = el
+    console.log(lastElement);
+    if (text.endsWith() === lastElement || text.endsWith() === lastElement || text.endsWith() === lastElement || text.endsWith() === lastElement) {
+        display.value = text.slice(0, -1)
+    }
+    display.value += el
+}
 
-    })
+const removeElement = () => {
+    const display = document.querySelector('.display');
+    const text = display.value
+    display.value = text.slice(0, -1)
+}
 
-})();
+const cleanDisplay = () => {
+    const display = document.querySelector('.display')
+    display.value = ''
+}
+
+"use strict"
+const equal = () => {
+    const display = document.querySelector('.display');
+    display.value = eval(display.value)
+}
+
