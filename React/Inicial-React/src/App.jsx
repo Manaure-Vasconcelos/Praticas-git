a7e6d20cf780890639cbc637399aa751e1f0c26d
Manaure Vasconcelos <manaurevasc97@gmail.com>
2023-10-14 16:35:59 -0300

    lifecycle methods

--- React/Inicial-React/src/App.jsx
+++ React/Inicial-React/src/App.jsx
@@ -2,9 +2,30 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  /* Atribuindo um state diretamente; */
   state = {
     name: 'Manaure',
     count: 0,
+    people: [
+      {
+        id: 1,
+        name: 'Gilda',
+        sobrenome: 'sousa',
+        idade: 59,
+      },
+      {
+        id: 2,
+        name: 'Millene',
+        sobrenome: 'soares',
+        idade: 23,
+      },
+      {
+        id: 3,
+        name: 'Manaure',
+        sobrenome: 'vasconcelos',
+        idade: 25,
+      },
+    ],
   };
 
   incrementCount = () => {
@@ -17,12 +38,40 @@ class App extends Component {
 
   mudarNome = () => {
     const inputName = document.querySelector('.inputName');
+    if (inputName.value === '') return;
     this.setState({ name: inputName.value });
     inputName.value = '';
-  };
+  }; 
+
+  /* Lifecycle methods */
+  timeoutUpdate = null;
+  // Quando o componente é renderizado.
+  componentDidMount() {
+    const inputName = document.querySelector('.inputName');
+    inputName.value = 'Atualizou';
+    this.handleTimeOut()
+  }
+  // Quando o componente é atualizado.
+  componentDidUpdate() {
+    this.handleTimeOut()
+  }
+  // Quando o componente vai ser alterado.
+  componentWillUnmount () {
+    clearInterval(this.timeoutUpdate)
+  }
+
+  handleTimeOut() {
+    const { count } = this.state;
+    
+    this.timeoutUpdate = setTimeout(() => {
+      
+      this.setState({ count: count + 1 });
+    }, 1000);
+  }
 
   render() {
-    const { name, count } = this.state;
+    /* Desestruturação */
+    const { name, count, people } = this.state;
 
     return (
       <>
@@ -39,6 +88,15 @@ class App extends Component {
         <div>
           <input type="text" className="inputName"></input> <br></br>
           <button onClick={this.mudarNome}>Mudar nome</button>
+        </div>
+
+        {/* State com arrays */}
+        <div>
+          {people.map((peoples) => (
+            <div key={peoples.id}>
+              <p>{peoples.name}</p>
+            </div>
+          ))}
         </div>
       </>
     );
